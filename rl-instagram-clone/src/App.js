62f5da3ea7ef34ece548b2e02dc7e70ff9e8162c
Rickielive <51518392+Rickielive@users.noreply.github.com>
2020-09-18 22:27:19 +0300

    2nd Commit

--- rl-instagram-clone/src/App.js
+++ rl-instagram-clone/src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Post from "./Post";
 
 function App() {
+  const [post, setPosts] = useState([
+    {
+      username: "Wachira",
+      caption: "Wow! Cheki hiyo drink",
+      imageUrl:
+        "https://images.pexels.com/photos/338713/pexels-photo-338713.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    },
+    {},
+  ]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -12,11 +22,22 @@ function App() {
           alt=""
         />
       </div>
-      <h1> Welcome Back Bazu</h1>
-      <Post />
-      {/* header */}
-      {/* posts */}
-      {/* posts */}
+
+      <Post
+        username="Wachira"
+        caption="Wow! Cheki hiyo drink"
+        imageUrl="https://images.pexels.com/photos/338713/pexels-photo-338713.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      />
+      <Post
+        username="Ann"
+        caption="Cheki hiyo pic"
+        imageUrl="https://images.pexels.com/photos/2600338/pexels-photo-2600338.jpeg?h=750&w=550&dpr=1&auto=compress"
+      />
+      <Post
+        username="Muthee"
+        caption=" Classic Kenyans"
+        imageUrl="https://images.pexels.com/photos/1300574/pexels-photo-1300574.jpeg?h=750&w=550&dpr=1&auto=compress"
+      />
     </div>
   );
 }
--- rl-instagram-clone/src/Post.js
+++ rl-instagram-clone/src/Post.js
@@ -2,28 +2,25 @@ import React from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
   return (
     <div className="post">
       <div className="post__header">
         <Avatar
           className="post__avatar"
           alt="Rickielive" //{username}
-          src="/static/images/avatar/1.jpg"
+          src={"/static/images/avatar/1.jpg"}
         />
-        <h3>Rickielive</h3>
+        <h3>{username}</h3>
       </div>
 
       {/* header => avatar and username */}
 
-      <img
-        className="post__image"
-        src="https://i.morioh.com/644d5dcb49.png"
-        alt=""
-      />
+      <img className="post__image" src={imageUrl} alt="" />
 
       <h4 className="post__text">
-        <strong>Rickielive :</strong>Wow! 3rd React js project
+        <strong>{username} :</strong>
+        {caption}
       </h4>
     </div>
   );
